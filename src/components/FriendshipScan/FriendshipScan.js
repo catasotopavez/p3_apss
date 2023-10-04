b8df2fc9ac685987403d59e9a7cce29d2488d1bd
catasotopavez <catasotopavez@Catalinas-MacBook-Pro.local>
2023-10-03 22:59:12 -0300

    changes

--- src/components/FriendshipScan/FriendshipScan.js
+++ src/components/FriendshipScan/FriendshipScan.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import MuiAlert from "@mui/material/Alert";
 import { QrReader } from 'react-qr-reader'
 import { Container } from '@mui/material'
 
-function FriendsPage(props) {
+function FriendshipScan(props) {
   const [linkInput, setLinkInput] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const authToken = localStorage.getItem('AUTH_TOKEN');
 
+
   const showAlert = (message) => {
     alert(message);
-}
+  }
 
   const closeNotification = () => {
     // Agrega lógica para cerrar la notificación aquí si es necesario
@@ -20,26 +20,65 @@ function FriendsPage(props) {
     setErrorMessage('');
   };
 
+  const getCurrentPositionAsync = () => {
+    return new Promise((resolve,reject)=>{
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          resolve(position)
+        },
+        (error) => {
+          reject(error)
+        }
+      )
+    })
+  }
 
-  const sendFriendRequest = (result) => {
+  const sendFriendRequest = async (result) => {
     const urlParams = new URLSearchParams(result);
     const friendId = urlParams.get('user_id');
-    console.log(urlParams)
+    const friendshipToken = urlParams.get('friendship_token');
+    console.log('friendshipToken:', friendshipToken);
+
     if (friendId) {
-      const apiUrl = `https://p1apps-production.up.railway.app/api/v1/friendships/${friendId}`;
+      const apiUrl = `https://p1apps-production.up.railway.app/api/v1/friendships`;
+      
+      if (friendshipToken) {
+        const timestamp = parseInt(friendshipToken, 10);
+        const date = new Date(timestamp * 1000);
+        const formattedDate = date.toLocaleString();
+
+        // Obtén la hora actual en segundos (timestamp)
+        const currentTimestamp = Math.floor(Date.now() / 1000);
+
+        // Calcula la diferencia de tiempo en segundos
+        const timeDifference = currentTimestamp - timestamp;
+
+        // Verifica si ha pasado más de un minuto (60 segundos)
+        if (timeDifference >= 60) {
+          showAlert('Ha pasado más de un minuto desde la creación del QR. Debes generar otro QR');
+          // No hagas el fetch y regresa
+          return;
+        }
+      }
+      const position = await getCurrentPositionAsync();
+      const {latitude,longitude} = position.coords;
+
+
+      const to_send = {"friendId":friendId,"gps_coord":{"latitude":latitude,"longitude":longitude}}
+      const dataJson = JSON.stringify(to_send)
       fetch(apiUrl, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${authToken}`
         },
+        body:dataJson
       })
         .then(async (response) => {
           if (response.ok) {
             const data = await response.json();
             setSuccessMessage(data.message);
             setErrorMessage('');
-
           } else {
             const errorData = await response.json();
             setErrorMessage(`Error al crear la amistad: ${errorData.error}`);
@@ -56,8 +95,8 @@ function FriendsPage(props) {
 
   return (
     <div style={{ padding: '20px', width: '600px', marginTop: '60px', display: 'flex', flexDirection: 'column' }}>
-    <h1>Escanea el QR</h1>
-    {successMessage && (
+      <h1>Escanea el QR</h1>
+      {successMessage && (
         <MuiAlert elevation={6} variant="filled" onClose={closeNotification} severity="success" >
           {successMessage}
         </MuiAlert>
@@ -67,26 +106,23 @@ function FriendsPage(props) {
           {errorMessage}
         </MuiAlert>
       )}
-      <Container
-        >
-            <QrReader
-                onResult={(result, error) => {
-                    if (!!result) {
-                        setLinkInput(result?.text);
-                        showAlert(`¡Código correctamente escaneado!`);
-                        sendFriendRequest(result.text);
-                    }
+      <Container>
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setLinkInput(result?.text);
+              showAlert(`¡Código correctamente escaneado!`);
+              sendFriendRequest(result.text);
+            }
 
-                    if (!!error) {
-                        
-                    }
-                }}
-              
-            />
-        </Container>
-      
+            if (!!error) {
+              // Manejar el error si es necesario
+            }
+          }}
+        />
+      </Container>
     </div>
   );
 }
 
-export default FriendsPage;
+export default FriendshipScan;
